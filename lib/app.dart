53e8f56c34bea59ba4d847553e1ec824276df91c
Sergei Vasilevkin <svasilevkin@gmail.com>
2021-05-27 10:47:32 +0300

    Delete template code.

---
+++ lib/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CanvasApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+      ),
+    );
+  }
+}
